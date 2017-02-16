153882f27bd0064e949269989906c370bb31e3a7
Avighan Majumder <dpn.majumder@gmail.com>
2017-02-16 11:10:49 +0530

    This is final

--- assignment_1/js/app.js
+++ assignment_1/js/app.js
@@ -8,20 +8,21 @@
       $scope.style = 'color:black; border-color: black';
       $scope.message = 'Please enter data first';
       $scope.show_or_hide = '';
+      $scope.message_style = '';
       $scope.displayMessages = function(){
       		if (splitString($scope.lunch,',') == 0){
       			$scope.message =  'Please enter data first';
       			$scope.style ='color: green;';
-      			
+      			$scope.message_style = 'color: red;';
 
       		}else if (splitString($scope.lunch,',') <= 3) {
       			$scope.message =  'Enjoy!';
-      			
+      			$scope.message_style = 'color: green;';
       			$scope.style = 'border-color: green';
       		}else if (splitString($scope.lunch,',') > 3) {
       			$scope.message =  'Too much!';
       			$scope.style = 'border-color: red';
-      			
+      			$scope.message_style = 'color: #999900;';
       		}
       		if ($scope.lunch.length >= 1 && splitString($scope.lunch,',') == 0){
       			$scope.show_or_hide = '1';
@@ -36,5 +37,3 @@
   		return arrayOfStrings.filter(String).length;
   	}
 };
-    
-    
